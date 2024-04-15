26b8c74c738c7b089839dae76e6f7d1654c08adf
nancysagar <nancysagat4726@gmail.com>
2024-04-15 19:18:32 +0530

    Stitched code

--- MentalFitnessTracker-main/app.py
+++ MentalFitnessTracker-main/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 from sklearn.preprocessing import LabelEncoder
-import pickle
+import joblib
 
 app = Flask(__name__)
 
 # Load your trained regression model
-#model = pickle.load(open("model.sav", "rb"))
+model = joblib.load("model.sav")
 
 # Home page
 @app.route('/')
@@ -26,28 +26,27 @@ def predict():
     if request.method == 'POST':
         try:
             # Get input features from the form
-             inputQuery1 = request.form['query1']
-             inputQuery2 = request.form['query2']
-             inputQuery3 = request.form['query3']
-             inputQuery4 = request.form['query4']
-             inputQuery5 = request.form['query5']
-             inputQuery6 = request.form['query6']
-             inputQuery7 = request.form['query7']
-             inputQuery8 = request.form['query8']
-             inputQuery9 = request.form['query9']
+            inputQuery1 = request.form['query1']
+            inputQuery2 = request.form['query2']
+            inputQuery3 = request.form['query3']
+            inputQuery4 = request.form['query4']
+            inputQuery5 = request.form['query5']
+            inputQuery6 = request.form['query6']
+            inputQuery7 = request.form['query7']
+            inputQuery8 = request.form['query8']
+            inputQuery9 = request.form['query9']
 
+            # Perform label encoding for categorical feature
+            l = LabelEncoder()
+            Country_encoded = l.fit_transform([inputQuery1])
 
-            # Add more features as needed
-             l = LabelEncoder()
-             Country_encoded = l.fit_transform([inputQuery1])
             # Perform prediction using the loaded model
-             #prediction = model.predict([[Country_encoded[0], inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, 
-             #inputQuery8, inputQuery9]])[0]
+            prediction = model.predict([[Country_encoded[0], inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, inputQuery8, inputQuery9]])[0]
 
-             #return render_template('result.html', prediction=prediction)
+            return render_template('result.html', prediction=prediction)
         except Exception as e:
             error_message = str(e)
             return render_template('error.html', error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
